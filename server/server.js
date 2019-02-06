c505d1d99c932ea53130876e424f97263debf078
weld <contact@weldev.tech>
2019-02-06 16:45:14 +0100

    Simple log system
    Reporting all of the get requests to logs/yyyy-mm-dd.twt

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = //
 //                          SNGenerator  - by weld                             //
 //                           Main file's server                                //
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = //
 var Streamer = require('./class/Streamer').Streamer, NotMachingRequirements = require('./class/Streamer').NotMachingRequirements;
 var urlparser = require('url');
+var fs = require('fs');
+var date = require('date-and-time');
 var _TOKEN_ = 'xh6dvhkx4t45vcz7mxj9asbl7n2jvx';
 var _PORT_ = 8550;
 /* --- FUNCTION --- */
@@ -18,6 +18,7 @@ var getHandler = function (req, res) {
     try {
         /* fetch the request*/
         var queries = urlparser.parse(req.url, true).query;
+        log(req.method, req.connection.remoteAddress, req.url.path, JSON.stringify(queries));
         var s = new Streamer(queries.login, queries.popup, queries.notif);
         /* render a response */
         res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -44,12 +45,28 @@ var getHandler = function (req, res) {
         res.end(errMsg);
     }
 };
+/*time utility */
+/* @from https://hype.codes/how-get-current-date-javascript */
+var dateNow = function () {
+    return new Date().toJSON().slice(0, 10).replace(/-/g, '_');
+};
+/* logs all requests in a logfile */
+var log = function (reqType, IpAddr, reqUrl, queries) {
+    if (reqUrl === void 0) { reqUrl = '/'; }
+    if (queries === void 0) { queries = ''; }
+    var toLog = "[" + reqType.toUpperCase() + "] " + reqUrl + " \nFROM " + IpAddr + "\nWITH params = " + queries + "\n\n";
+    fs.writeFile('./logs/' + dateNow() + '.txt', toLog, { flag: 'a' }, function (err) {
+        if (err)
+            console.log(err);
+    });
+};
 /* --- MAIN --- */
 var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.method === 'GET')
         getHandler(req, res);
     else {
+        log(req.method, req.connection.remoteAddress);
         res.writeHead(405, { 'Content-Type': 'text/plain' });
         res.end('Method not allowed');
     }
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,3 @@
-import { NotMatchingRequirements } from "./class/Streamer";
-
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = //
 //                          SNGenerator  - by weld                             //
 //                           Main file's server                                //
@@ -7,12 +5,14 @@ import { NotMatchingRequirements } from "./class/Streamer";
 
 let Streamer = require('./class/Streamer').Streamer, NotMachingRequirements = require('./class/Streamer').NotMachingRequirements; 
 let urlparser = require('url'); 
+let fs = require('fs');
+let date = require('date-and-time');
 
 const _TOKEN_: string = 'xh6dvhkx4t45vcz7mxj9asbl7n2jvx';
 const _PORT_: number = 8550;
 
 /* --- FUNCTION --- */
-let getHandler = (req, res) => {
+let getHandler = (req, res): void => {
     /* allowing CORS */
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Request-Method', '*');
@@ -22,12 +22,14 @@ let getHandler = (req, res) => {
     try {
         /* fetch the request*/
         let queries = urlparser.parse(req.url, true).query;
+        log(req.method, req.connection.remoteAddress, req.url.path, JSON.stringify(queries));
         let s = new Streamer(queries.login, queries.popup, queries.notif);
 
-        
         /* render a response */
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('No error (:');
+
+        
     } catch (err) {
         let errMsg = err.name + ' ';
         switch(err.name) {
@@ -51,12 +53,34 @@ let getHandler = (req, res) => {
     }
 }
 
+/*time utility */
+/* @from https://hype.codes/how-get-current-date-javascript */
+let dateNow = (): string => {
+    return new Date().toJSON().slice(0,10).replace(/-/g,'_');
+}
+
+/* logs all requests in a logfile */
+let log = (reqType: string, IpAddr: string, reqUrl: string = '/', queries: string = ''): void => {
+    let toLog = `[${reqType.toUpperCase()}] ${reqUrl} 
+FROM ${IpAddr}
+WITH params = ${queries}
+
+`;
+    fs.writeFile('./logs/'+dateNow()+'.txt', toLog, {flag:'a'}, (err) => {
+        if(err)
+            console.log(err)
+    } );
+}
+
+
+
 /* --- MAIN --- */
 let http = require('http');
 let server = http.createServer((req, res) => {
     if(req.method === 'GET')
         getHandler(req, res);
     else {
+        log(req.method, req.connection.remoteAddress)
         res.writeHead(405, {'Content-Type': 'text/plain'});
         res.end('Method not allowed');
     }
@@ -64,3 +88,5 @@ let server = http.createServer((req, res) => {
 
 server.listen(_PORT_);
 console.log('[INFO] Server is now running');
+
+
